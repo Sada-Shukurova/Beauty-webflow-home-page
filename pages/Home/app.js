190aa20387b4b2c933083f8eb2423df46d9e6b76
sada shukurova <shukurova.sada@gmail.com>
2023-07-17 16:29:26 +0400

    home section1 with responsive

--- pages/Home/app.js
+++ pages/Home/app.js
@@ -1,14 +1,34 @@
 let menu = document.getElementById('menu')
-let navigation = document.querySelector('.navSocial-container')
+let navigation = document.querySelector('.nav-container')
 let menuClose = document.getElementById('menu-close')
-let headerContainer = document.querySelector(".header-container")
+
+
 
 menu.addEventListener("click", () => {
-    navigation.classList.add("menu-active")
-    headerContainer.classList.add("menu-active")
+    navigation.classList.add("menu-active");
+    menu.style.visibility = 'hidden';
+    document.body.classList.add('body-scroll-hidden')
 })
 
 menuClose.addEventListener('click', () => {
     navigation.classList.remove('menu-active')
-    headerContainer.classList.remove("menu-active")
+    document.body.classList.remove('body-scroll-hidden')
+    menu.style.visibility = 'visible'
 })
+
+
+
+let header = document.querySelector("header")
+window.addEventListener('scroll',
+    function handleScroll() {
+        if (window.scrollY < 40) {
+            navigation.style.top = '8%'
+            header.classList.remove("fixed")
+        } else if (window.scrollY > 40) {
+            navigation.style.top = '10%'
+            header.classList.add("fixed")
+        } if (window.scrollY > 70) {
+            navigation.style.top = '15%'
+        }
+
+    })
