bc8ebfe9b3d85b51c1a9f9b783d3279cc5035f85
sada shukurova <shukurova.sada@gmail.com>
2023-07-27 13:03:40 +0400

    Home page added

--- pages/Home/app.js
+++ pages/Home/app.js
@@ -1,7 +1,7 @@
-let menu = document.getElementById('menu')
-let navigation = document.querySelector('.nav-container')
-let menuClose = document.getElementById('menu-close')
-
+const menu = document.getElementById('menu')
+const navigation = document.querySelector('.nav-container')
+const menuClose = document.getElementById('menu-close')
+const dropdown = document.querySelector('.dropdown')
 
 
 menu.addEventListener("click", () => {
@@ -16,19 +16,21 @@ menuClose.addEventListener('click', () => {
     menu.style.visibility = 'visible'
 })
 
+dropdown.addEventListener('click', () => {
+    dropdown.classList.toggle("dropdown-open")
+})
 
+// let header = document.querySelector("header")
+// window.addEventListener('scroll',
+//     function handleScroll() {
+//         if (window.scrollY < 40) {
+//             navigation.style.top = '8%'
+//             header.classList.remove("fixed")
+//         } else if (window.scrollY > 40) {
+//             navigation.style.top = '10%'
+//             header.classList.add("fixed")
+//         } if (window.scrollY > 70) {
+//             navigation.style.top = '15%'
+//         }
 
-let header = document.querySelector("header")
-window.addEventListener('scroll',
-    function handleScroll() {
-        if (window.scrollY < 40) {
-            navigation.style.top = '8%'
-            header.classList.remove("fixed")
-        } else if (window.scrollY > 40) {
-            navigation.style.top = '10%'
-            header.classList.add("fixed")
-        } if (window.scrollY > 70) {
-            navigation.style.top = '15%'
-        }
-
-    })
+//     })
